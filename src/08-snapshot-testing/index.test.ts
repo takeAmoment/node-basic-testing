0c4caeb6fec2543b4db5d5e3d1f58d1d3e78492f
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-20 18:22:19 +0200

    feat: add snapshot testing tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,24 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const values = [1];
+    const expectedList = {
+      next: {
+        next: null,
+        value: null,
+      },
+      value: 1,
+    }
+    expect(generateLinkedList(values)).toStrictEqual(expectedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const values = [4, 5];
+    const result = generateLinkedList(values);
+    
+    expect(result).toMatchSnapshot();
   });
 });
